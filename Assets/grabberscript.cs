b8a13dec550fbbdaf09d9bc76ae2654a8655aab7
Dian Anggraini <yandiyan29@gmail.com>
2016-08-16 12:40:37 +0700

    Add files via upload

---
+++ Assets/grabberscript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grabberscript : MonoBehaviour {
+
+	// Use this for initialization
+    public bool grabbed;
+	RaycastHit2D hit;
+	public float distance=2f;
+	public Transform holdpoint;
+	public float throwforce;
+	public LayerMask notgrabbed;
+
+    public GameObject papanmuncul;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.B))
+		{
+
+			if(!grabbed)
+			{
+              // Physics2D.raycastsStartInColliders = false;
+				Physics2D.queriesStartInColliders=false;
+
+			hit =	Physics2D.Raycast(transform.position,Vector2.right*transform.localScale.y,distance);
+
+            if (hit.collider != null && hit.collider.tag == "grabbable")
+            {
+                grabbed = true;
+
+            }
+
+
+				//grab
+			}
+
+            else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
+            {
+                grabbed = false;
+
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                    papanmuncul.SetActive(true);
+                }
+
+
+                //throw
+            }
+
+
+		}
+
+        if (grabbed)
+            hit.collider.gameObject.transform.position = holdpoint.position;
+
+	
+	}
+void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position,transform.position+Vector3.right*transform.localScale.x*distance);
+	}
+}
+
+
---
+++ Assets/kelincikejar.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class kelincikejar : MonoBehaviour {
+
+    public GameObject target;
+    public float ratio = 0.05f;
+	// Use this for initialization
+
+    public bool isDead;
+    public GameObject restartText;
+
+	
+	void Start () {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("ldg");
+            
+        }
+       // anim = GetComponent<Animator>();
+      //  rb = GetComponent<Rigidbody2D>();
+       
+	}
+    //IEnumerator ImmuneTime()
+    //{
+    //    yield return new WaitForSeconds(immuneTime);
+    //    isImmune = false;
+
+    // //   anim.SetBool("Immune", false);
+    //}
+        
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, ratio);
+        //SceneManager.LoadScene("misigagal");
+        if (isDead == true) {
+
+        //    restartText.SetActive(true);
+            SceneManager.LoadScene("misigagal");
+		
+                        //if (Input.GetButtonDown ("Restart"))
+                        //        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			
+			
+                        //return;
+			
+				}
+
+	}
+ 
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "ldg" && !isDead)
+        {
+          //  rb.velocity = Vector2.zero;
+           // lives = 0;
+            StartCoroutine("ImmuneTime");
+          //  anim.SetBool("Dies", true);
+           // rb.AddForce(new Vector2(0, 500));
+            isDead = true;
+
+
+        }
+        else if (other.tag == "ldg")
+        {
+           // lives--;
+          //  anim.SetBool("Immune", true);
+          //  StartCoroutine("ImmuneTime");
+         //   isImmune = true;
+        }
+
+    }
+
+
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ldg" && !isDead )
+        {
+           // rb.velocity = Vector2.zero;
+           // lives = 0;
+       //     anim.SetBool("Dies", true);
+        //    rb.AddForce(new Vector2(0, 500));
+            isDead = true;
+
+
+        }
+        else if (other.gameObject.tag == "ldg")
+        {
+         //   lives--;
+         //   anim.SetBool("Immune", true);
+         //   isImmune = true;
+        }
+
+    }
+
+
+
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+      //  outsideForce = false;
+
+
+       // if (other.gameObject.tag == "Platform")
+         //   BaseSpeed = other.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+
+
+    }
+
+}
+
